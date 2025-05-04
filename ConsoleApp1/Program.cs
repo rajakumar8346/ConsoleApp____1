8df85d1dd821abfd7b255385a7a42c90542e012e
Raja Kumar <rajkumar.dongiri@betalen.in>
2025-05-04 09:44:46 +0530

    Created a one class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,4 +16,17 @@ namespace ConsoleApp1
             Console.ReadLine();
         }
     }
+    public class test
+    {
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+        public int e;
+        public int f;
+        public int g;
+        public int h;
+        public int i;
+        public int j;
+    }
 }
